0285caed89f813bf3ec790ce254d012ec6a28284
Alexey Shekhirin <5773434+shekhirin@users.noreply.github.com>
2025-06-17 19:32:12 +0100

    fix CI

---
+++ nybbles-bench/src/lib.rs
@@ -0,0 +1,7 @@
+//! Benchmarks for the `nybbles` crate.
+//!
+//! This is a separate crate to avoid building unnecessary dev-dependencies and
+//! the test suite when running these benchmarks in CI.
+
+#[cfg(test)]
+fn main() {}
