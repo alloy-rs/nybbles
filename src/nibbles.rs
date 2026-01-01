cd2535c13a79bb9b5a837d6250d55c79bf9bc516
figtracer <1gusredo@gmail.com>
2026-01-01 16:34:10 +0000

    fmt

--- src/nibbles.rs
+++ src/nibbles.rs
@@ -423,8 +423,8 @@ impl Nibbles {
         Self { length, nibbles }
     }
 
-    /// Converts a fixed 32 byte array into a [`Nibbles`] instance. Similar to [`Nibbles::unpack`], but
-    /// is not `unsafe`.
+    /// Converts a fixed 32 byte array into a [`Nibbles`] instance. Similar to [`Nibbles::unpack`],
+    /// but is not `unsafe`.
     #[inline]
     pub const fn unpack_array(data: &[u8; 32]) -> Self {
         let nibbles = U256::from_be_bytes(*data);
@@ -662,7 +662,11 @@ impl Nibbles {
     /// Returns the nibble at the given index.
     #[inline]
     pub fn get(&self, i: usize) -> Option<u8> {
-        if self.check_index(i) { Some(self.get_unchecked(i)) } else { None }
+        if self.check_index(i) {
+            Some(self.get_unchecked(i))
+        } else {
+            None
+        }
     }
 
     /// Returns the nibble at the given index.
@@ -675,7 +679,11 @@ impl Nibbles {
     pub fn get_unchecked(&self, i: usize) -> u8 {
         self.assert_index(i);
         let byte = as_le_slice(&self.nibbles)[U256::BYTES - i / 2 - 1];
-        if i % 2 == 0 { byte >> 4 } else { byte & 0x0F }
+        if i % 2 == 0 {
+            byte >> 4
+        } else {
+            byte & 0x0F
+        }
     }
 
     /// Sets the nibble at the given index.
@@ -736,7 +744,11 @@ impl Nibbles {
     #[inline]
     pub fn last(&self) -> Option<u8> {
         let len = self.len();
-        if len == 0 { None } else { Some(self.get_unchecked(len - 1)) }
+        if len == 0 {
+            None
+        } else {
+            Some(self.get_unchecked(len - 1))
+        }
     }
 
     /// Returns the length of the common prefix between this nibble sequence and the given.
@@ -783,7 +795,11 @@ impl Nibbles {
             (self.nibbles ^ other.nibbles) & mask
         };
 
-        if xor == U256::ZERO { min_nibble_len } else { xor.leading_zeros() / 4 }
+        if xor == U256::ZERO {
+            min_nibble_len
+        } else {
+            xor.leading_zeros() / 4
+        }
     }
 
     /// Returns the total number of bits in this [`Nibbles`].
